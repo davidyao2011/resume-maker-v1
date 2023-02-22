ca4eaa2f8cc4b9cc064186b21393985a0cbad48c
David Yao <davidyao2011@hotmail.com>
2023-02-23 10:19:06 +1300

    Add interface pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Resume from "./components/Resume";
+
 function App() {
   return (
-    <div className="App">
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
