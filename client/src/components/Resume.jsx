da59c0faed08a43aecfa1d62146445fcbabd576a
David Yao <davidyao2011@hotmail.com>
2023-02-24 10:21:57 +1300

    Add multer to the server

--- client/src/components/Resume.jsx
+++ client/src/components/Resume.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import ErrorPage from './ErrorPage'
+import React from "react";
+import ErrorPage from "./ErrorPage";
 
-export default function Resume({result}) {
-  if(JSON.stringify(result) = "{}") {
-    return <ErrorPage/>
-  }
-  const handlePrint = () => alert('print is done successfully.');
+export default function Resume({ result }) {
+  // if(JSON.stringify(result) = "{}") {
+  //   return <ErrorPage/>
+  // }
+  // function handlePrint() {
+  //   return console.log('print is done successfully.');
+  // }
   return (
     <>
       <button onclick={handlePrint}>Print Page</button>
-      <main className='container'>
+      <main className="container">
         <p>Done!</p>
       </main>
     </>
-  )
+  );
 }
--- server/index.js
+++ server/index.js
@@ -2,10 +2,27 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = 4000;
+const multer = require("multer");
+const path = require("path");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
+app.use("/uploads", express.static("uploads"));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = mutler({
+  storage: storage,
+  limits: { filesize: 1024 * 1024 * 5 },
+});
 
 app.get("/", (req, res) => {
   res.json({
